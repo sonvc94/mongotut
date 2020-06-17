0ed2fe7b2220dd883e125c8e7c4e53db396f1629
sonvc <sonvc@vnpay.vn>
2020-06-17 11:06:31 +0700

    init project

---
+++ src/main/java/vn/vnpaya/mongotut/MongotutApplication.java
@@ -0,0 +1,13 @@
+package vn.vnpaya.mongotut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongotutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongotutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/vnpaya/mongotut/MongotutApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.vnpaya.mongotut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongotutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
